57e64180104508be180c4776f0f27ea440e28501
Wayne Zhang <waynezhang007@gmail.com>
2023-09-16 21:32:38 -0400

    add routes to get data from alphavantage

--- index.js
+++ index.js
@@ -1,11 +1,67 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Middleware to parse JSON bodies.
+app.use(express.json());
 
+// Parse urlencoded request body.
+app.use(express.urlencoded({ extended: true }));
+
+// GET income statement.
+app.get('/:ticker/incomestatement', async (req, res, next) => {
+  let ticker = req.params.ticker;
+
+  try {
+    let alphavantage = await fetch('https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=IBM&apikey=demo');
+    let data = await alphavantage.json();
+  } catch (err) {
+    next(err);
+  }
+  
+  res.json(data);
+});
+
+// GET balance sheet.
+app.get('/:ticker/balancesheet', async (req, res, next) => {
+  let ticker = req.params.ticker;
+
+  try {
+    let alphavantage = await fetch('https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=IBM&apikey=demo');
+    let data = await alphavantage.json();
+  } catch (err) {
+    next(err);
+  }
+  
+  res.json(data);
+});
+
+// GET cash flow.
+app.get('/:ticker/cashflow', async (req, res, next) => {
+  let ticker = req.params.ticker;
+
+  try {
+    let alphavantage = await fetch('https://www.alphavantage.co/query?function=CASH_FLOW&symbol=IBM&apikey=demo');
+    let data = await alphavantage.json();
+  } catch (err) {
+    next(err);
+  }
+
+  res.json(data);
+});
+
+// 404 handling middleware.
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Not found' });
+});
+
+// 500 handling middleware.
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Start app.
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`);
 })
